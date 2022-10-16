c78ff617ac3c4021b8387ab917c6e5f08546f7e9
Ubugeeei <ubuge1122@gamil.com>
2022-10-16 15:36:08 +0900

    emit: function

---
+++ c/function/main.c
@@ -0,0 +1,8 @@
+int hun_mul(int x) { return x * 100; }
+
+int main() {
+    int x = 5;
+    int y = hun_mul(x);
+
+    return y;
+}
---
+++ c/function/main.s
@@ -0,0 +1,41 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 10, 15	sdk_version 10, 15, 6
+	.globl	_hun_mul                ## -- Begin function hun_mul
+	.p2align	4, 0x90
+_hun_mul:                               ## @hun_mul
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	movl	%edi, -4(%rbp)
+	imull	$100, -4(%rbp), %eax
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+	.globl	_main                   ## -- Begin function main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movl	$0, -4(%rbp)
+	movl	$5, -8(%rbp)
+	movl	-8(%rbp), %edi
+	callq	_hun_mul
+	movl	%eax, -12(%rbp)
+	movl	-12(%rbp), %eax
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+.subsections_via_symbols
